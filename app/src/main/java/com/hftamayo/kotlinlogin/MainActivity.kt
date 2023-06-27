626f7ac1c4555581badea8eb09c702542cfcdb8d
hftamayo <hftamayo@gmail.com>
2023-06-27 09:31:13 -0600

    finishing UI stage

--- app/src/main/java/com/hftamayo/kotlinlogin/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinlogin/MainActivity.kt
@@ -2,10 +2,37 @@ package com.hftamayo.kotlinlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hftamayo.kotlinlogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initialize the UI
+        initUI()
     }
+
+    private fun initUI() {
+        binding.fabBack.setOnClickListener {
+
+        }
+
+        binding.btnSignin.setOnClickListener {
+
+        }
+
+        binding.tvFogotPassword.setOnClickListener {
+
+        }
+
+        binding.tvSignup.setOnClickListener {
+
+        }
+    }
+
+
 }
